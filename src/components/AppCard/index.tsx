ab60caa8dc018be66240bbdbd22be853c6b6c97a
Lucas Moreno <lmorenooitabem@gmail.com>
2025-11-26 15:40:13 -0300

    feat: creat appCard

---
+++ src/components/AppCard/index.tsx
@@ -0,0 +1,16 @@
+import "./Styles.css";
+
+export default function AppCard() {
+  return (
+    <div className="app-card">
+      <div className="card-row">
+        <h2 >SIMTECH</h2>
+        <p id="label-left">07/12 - 11/12</p>
+      </div>
+      <p className="content">
+        O evento que ocorrerá no IME, conta com palestras, stands e diversas
+        atividade para os estudantes do IC e de fora também,
+      </p>
+    </div>
+  );
+}
