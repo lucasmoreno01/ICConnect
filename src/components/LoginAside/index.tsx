cfec9c246dee0bfaccf856824790025d975db9cd
Lucas Moreno <lmorenooitabem@gmail.com>
2025-11-29 09:03:06 -0300

    fix: adjust margins

--- src/components/LoginAside/index.tsx
+++ src/components/LoginAside/index.tsx
@@ -2,7 +2,7 @@ import "./Styles.css";
 export default function LoginAside() {
   return (
     <>
-      <aside>
+      <aside id="login-aside">
         <h1>ICConect</h1>
         <p>Sistema de Apoio ao Estudante do IC UFBA</p>
       </aside>
